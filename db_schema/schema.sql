e600774f66fe0e60934f944566f25c6ccc645d4a
Pedro <zs10011598@gmail.com>
2020-01-29 20:39:11 -0600

    se agrega tabla de escuela y se agrgan datos personales a tabla de profes

--- db_schema/schema.sql
+++ db_schema/schema.sql
@@ -44,3 +44,53 @@ CREATE TABLE localidad(
 
 CREATE INDEX idx_localidad_id ON localidad(id);
 CREATE INDEX idx_localidad_id_municipio ON localidad(id_municipio);
+
+CREATE TABLE escuela(
+					id serial PRIMARY KEY,
+					nombre varchar(300),
+					servicios_regionales varchar(200),
+					clave varchar(100),
+					zona varchar(50),
+					sector varchar(50),
+					organismo_publico varchar(200),
+					calle_y_numero varchar(300),
+					colonia varchar(200),
+					cp varchar(50),
+					id_estado integer REFERENCES estado(id),
+					id_municipio integer REFERENCES municipio(id),
+					id_localidad integer REFERENCES localidad(id)
+					);
+
+CREATE INDEX idx_escuela_id ON escuela(id);
+CREATE INDEX idx_escuela_nombre ON escuela(nombre);
+CREATE INDEX idx_escuela_id_estado ON escuela(id_estado);
+CREATE INDEX idx_escuela_id_municipio ON escuela(id_municipio);
+CREATE INDEX idx_escuela_id_localidad ON escuela(id_localidad);
+
+CREATE TABLE rel_profe_escuela(
+								id serial,
+								id_profe integer REFERENCES profe(id),
+								id_escuela integer REFERENCES escuela(id)
+							);
+
+CREATE INDEX idx_rel_profe_escuela_id_profe ON rel_profe_escuela(id_profe);
+CREATE INDEX idx_rel_profe_escuela_id_escuela ON rel_profe_escuela(id_escuela);
+
+CREATE TYPE sexo AS ENUM ('M', 'F');
+
+ALTER TABLE profe ADD COLUMN fecha_nacimiento timestamp;
+ALTER TABLE profe ADD COLUMN telefono varchar(50);
+ALTER TABLE profe ADD COLUMN sexo sexo;
+ALTER TABLE profe ADD COLUMN calle_y_numero varchar(300);
+ALTER TABLE profe ADD COLUMN colonia varchar(200);
+ALTER TABLE profe ADD COLUMN cp varchar(50);
+ALTER TABLE profe ADD COLUMN id_estado integer REFERENCES estado(id) DEFAULT null;
+ALTER TABLE profe ADD COLUMN id_municipio integer REFERENCES municipio(id) DEFAULT null;
+ALTER TABLE profe ADD COLUMN id_localidad integer REFERENCES localidad(id) DEFAULT null;
+
+CREATE INDEX idx_profe_fecha_nacimiento ON profe(fecha_nacimiento);
+CREATE INDEX idx_profe_fecha_sexo ON profe(fecha_sexo);
+CREATE INDEX idx_profe_fecha_cp ON profe(cp);
+CREATE INDEX idx_profe_id_estado ON profe(id_estado);
+CREATE INDEX idx_profe_id_municipio ON profe(id_municipio);
+CREATE INDEX idx_profe_id_localidad ON profe(id_localidad);
